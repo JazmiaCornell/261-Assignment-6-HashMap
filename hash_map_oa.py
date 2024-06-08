e5d222a4e6d93a99e02ceb9aa7321292d3c7157a
Jazmia Cornell <jazmiad@gmail.com>
2024-06-08 18:06:05 -0400

    remove test_5

--- hash_map_oa.py
+++ hash_map_oa.py
@@ -151,7 +151,7 @@ class HashMap:
             bucket = self._buckets.get_at_index(i)
             # if bucket found, calculates new index (to move value to new array)
             if bucket and not bucket.is_tombstone:
-                index = self._hash_function(bucket.key) % new_capacity
+                index = self._hash_function(bucket.key) % self._capacity
                 j = 0
                 while True:
                     quad_prob = (index + (j ** 2)) % new_capacity
